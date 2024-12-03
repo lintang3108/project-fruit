b01b600c7cc35571c4c1e72d6ed98d63a037dd80
lintang3108 <setiaalintang@gmail.com>
2024-12-03 10:42:42 +0700

    first commit

---
+++ src/main/java/com/example/belajar/apiCrud/controllers/CarController.java
@@ -0,0 +1,14 @@
+package com.example.belajar.apiCrud.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/car")
+public class CarController {
+
+   @GetMapping(value = "checkAPI")
+    public String checkApi() {
+       return "Hello Word";
+   }
+}
